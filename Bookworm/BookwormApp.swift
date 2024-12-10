0fe10115fd57b1ee25083cc7461efc6bfc8f06cc
Constantin Lisnic <constantin@lisnic.dev>
2024-12-10 09:24:16 +0000

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Constantin Lisnic on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookwormTests/BookwormTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormTests.swift
+//  BookwormTests
+//
+//  Created by Constantin Lisnic on 10/12/2024.
+//
+
+import Testing
+@testable import Bookworm
+
+struct BookwormTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
